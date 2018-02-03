6fd0ce11b6155b5ad410c06270fbd1d970ddbb8c
Maiko Cunha <mkacunha@gmail.com>
2018-02-03 14:38:11 -0200

    Backend - spring boot

---
+++ warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/WarmersCupServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.mkacunha.warmerscup.warmerscupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmersCupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmersCupServerApplication.class, args);
+	}
+}
