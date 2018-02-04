39d4df8cff1d1a530dc1c172166e66eb24bdcfbb
Maiko Cunha <mkacunha@gmail.com>
2018-02-04 16:02:52 -0200

    Configuração do banco de dados mysq-sql

--- warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/Application.java
+++ warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WarmersCupServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarmersCupServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
+
 }
