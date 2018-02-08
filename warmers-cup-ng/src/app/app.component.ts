b1ad149e140c7dc6216c1c8360fbceab6cc628eb
Maiko Cunha <mkacunha@gmail.com>
2018-02-07 22:16:40 -0200

    Implementado um demo de upload de imagem e preview no componente principal

--- warmers-cup-ng/src/app/app.component.ts
+++ warmers-cup-ng/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  file: File;
+  fileUrl: string;
+
+  selectFile(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.file = event.target.files[0];
+
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.fileUrl = e.target.result;
+      };
+
+      reader.readAsDataURL(this.file);
+    }
+  }
 }
