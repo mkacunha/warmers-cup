0592fb22e41ea6840c34d8467917b240acf50952
Maiko Cunha <mkacunha@gmail.com>
2018-02-09 21:51:35 -0200

    Criado componentes e configurado rotas

--- warmers-cup-ng/src/app/app.module.ts
+++ warmers-cup-ng/src/app/app.module.ts
@@ -6,12 +6,22 @@ import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { PlayerComponent } from './player/player.component';
+import { ScoreComponent } from './score/score.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    TeamComponent,
+    PlayerComponent,
+    ScoreComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule,
--- warmers-cup-ng/src/app/app.routes.ts
+++ warmers-cup-ng/src/app/app.routes.ts
@@ -1,8 +1,18 @@
+import { TeamComponent } from './team/team.component';
+import { PlayerComponent } from './player/player.component';
+import { ScoreComponent } from './score/score.component';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 
 const appRoutes: Routes = [
-  { path: '**', redirectTo: 'home' }
+    { path: 'home', component: HomeComponent },
+    { path: 'team', component: TeamComponent },
+    { path: 'player', component: PlayerComponent },
+    { path: 'score', component: ScoreComponent },
+    { path: 'presentation', component: PresentationComponent },
+    { path: '**', redirectTo: 'home' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
