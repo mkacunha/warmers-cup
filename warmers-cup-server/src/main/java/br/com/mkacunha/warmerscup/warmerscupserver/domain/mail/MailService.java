3b24d15bc568b51943b28adf56a2134c30159491
Maiko Cunha <maiko.cunha@db1.com.br>
2018-03-02 17:44:31 -0300

    Implementado envio de e-mail de cadastro para usuários remotos

---
+++ warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/domain/mail/MailService.java
@@ -0,0 +1,35 @@
+package br.com.mkacunha.warmerscup.warmerscupserver.domain.mail;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailService {
+
+	private final JavaMailSender emailSender;
+
+	public MailService(JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+
+	@Async
+	public void send(String message, String mail) {
+		try {
+			System.out.println("Enviando E-mail para " + mail);
+			MimeMessage mimeMessage = emailSender.createMimeMessage();
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+			mimeMessageHelper.setSubject("Copa dos Agasalhos :):)");
+			mimeMessageHelper.setTo(mail);
+			mimeMessageHelper.setText(message, true);
+			emailSender.send(mimeMessage);
+			System.out.println("Email enviado para " + mail);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
--- warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/domain/player/Player.java
+++ warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/domain/player/Player.java
@@ -12,101 +12,108 @@ import static java.lang.Boolean.FALSE;
 @Entity
 public class Player {
 
-    @Id
-    private String id;
+	@Id
+	private String id;
 
-    private String name;
+	private String name;
 
-    private String email;
+	private String email;
 
-    private String hash;
+	private String hash;
 
-    private Boolean remote = FALSE;
+	private Boolean remote = FALSE;
 
-    @ManyToOne
-    private Team team;
+	@ManyToOne
+	private Team team;
 
-    private Player(Builder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.email = builder.email;
-        this.hash = builder.hash;
-        this.team = builder.team;
-        this.team.plusNumberPlayer();
-    }
+	private Player(Builder builder) {
+		this.id = builder.id;
+		this.name = builder.name;
+		this.email = builder.email;
+		this.hash = builder.hash;
+		this.team = builder.team;
+		this.team.plusNumberPlayer();
+		this.remote = builder.remote;
+	}
 
-    protected Player() {
-    }
+	protected Player() {
+	}
 
+	static Builder builder() {
+		return new Builder();
+	}
 
-    static Builder builder() {
-        return new Builder();
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getHash() {
+		return hash;
+	}
 
-    public String getHash() {
-        return hash;
-    }
+	public Team getTeam() {
+		return team;
+	}
 
-    public Team getTeam() {
-        return team;
-    }
+	public Boolean getRemote() {
+		return remote;
+	}
 
-    public Boolean getRemote() {
-        return remote;
-    }
+	static class Builder {
 
-    static class Builder {
+		private String id;
 
-        private String id;
+		private String name;
 
-        private String name;
+		private String email;
 
-        private String email;
+		private String hash;
 
-        private String hash;
+		private Team team;
 
-        private Team team;
+		private Boolean remote = FALSE;
 
-        private Builder() {
-            this.id = UUID.randomUUID().toString();
-        }
+		private Builder() {
+			this.id = UUID.randomUUID().toString();
+		}
 
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
+		public Builder name(String name) {
+			this.name = name;
+			return this;
+		}
 
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
+		public Builder email(String email) {
+			this.email = email;
+			return this;
+		}
 
-        public Builder hash(String hash) {
-            this.hash = hash;
-            return this;
-        }
+		public Builder hash(String hash) {
+			this.hash = hash;
+			return this;
+		}
 
-        public Builder team(Team team) {
-            this.team = team;
-            return this;
-        }
+		public Builder team(Team team) {
+			this.team = team;
+			return this;
+		}
 
-        public Player build() {
-            return new Player(this);
-        }
+		public Builder remote(Boolean remote) {
+			this.remote = remote;
+			return this;
+		}
 
-    }
+		public Player build() {
+			return new Player(this);
+		}
+
+	}
 
 }
