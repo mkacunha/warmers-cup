c56cd8d091c7f837b05b7014461bb2871b81829c
Maiko Cunha <maiko.cunha@db1.com.br>
2018-03-05 16:58:28 -0300

    Adicionado url de consulta por rfid

--- warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/domain/worker/WorkerService.java
+++ warmers-cup-server/src/main/java/br/com/mkacunha/warmerscup/warmerscupserver/domain/worker/WorkerService.java
@@ -6,22 +6,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import static java.util.Objects.isNull;
+
 @Service
 public class WorkerService {
 
-	private static final String URL = "http://192.168.208.164:32568/api/collaborator/getbyname/?collaboratorName=";
+	private static final String URL = "http://192.168.208.164:32568/api/collaborator/getbyrfid/?rfid=%s";
 
 	public WorkerDTO findByHash(String hash) {
-        /*WorkerDTO worker = new WorkerDTO();
-        worker.setName("Name " + hash);
-        worker.setHash(hash);
-        worker.setEmail(hash + "@email.com");
-        return worker;*/
-
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<WorkerDTO> user = restTemplate
-				.exchange(URL + "", HttpMethod.GET, null, new ParameterizedTypeReference<WorkerDTO>() {
+				.exchange(String.format(URL, hash), HttpMethod.GET, null, new ParameterizedTypeReference<WorkerDTO>() {
 				});
+
+		if (isNull(user.getBody())) {
+			throw new RuntimeException("Usuário não cadastrado na base de dados");
+		}
+
 		return user.getBody();
 	}
 }
